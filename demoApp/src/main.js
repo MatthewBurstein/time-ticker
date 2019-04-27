ca1ae36b1601a6e0e6ebee058dc732c9a821a155
Matt Burstein <j.matthew.burstein@gmail.com>
2019-04-27 18:25:48 +0100

    feat: add basic ability to move around in demoApp

--- demoApp/src/main.js
+++ demoApp/src/main.js
@@ -1,3 +1,67 @@
 require('../public/main.scss')
 require("../src/index.html")
-require("../../src/index")
+
+import ticker from "../../src/index"
+import { mod } from './utils';
+
+window.ticker = ticker
+ticker.setPeriod(1000)
+
+const DIRECTIONS = {
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down'
+}
+const BOARD_DIMENSION = 9
+
+const $ = require('jquery')
+
+const state = {
+  head: [0,0],
+  direction: ''
+}
+
+
+$('window').ready(() => {
+  $('.page-container').keydown(function(e) {
+    state.direction = DIRECTIONS[e.keyCode]
+  })
+
+  const move = () => {
+    switch (state.direction) {
+      case 'left':
+      state.head[1] = state.head[1] === 0 ? BOARD_DIMENSION - 1 : state.head[1] - 1
+      break;
+      case 'up':
+      state.head[0] = state.head[0] === 0 ? BOARD_DIMENSION - 1 : state.head[0] - 1
+      break;
+      case 'right':
+      state.head[1] = state.head[1] === BOARD_DIMENSION - 1 ? 0 : state.head[1] + 1
+      break;
+      case 'down':
+      state.head[0] = state.head[0] === BOARD_DIMENSION - 1 ? 0 : state.head[0] + 1
+      break;
+    }
+  }
+
+  const tick = () => {
+    move()
+    render()
+  }
+
+  const render = () => {
+    const divIdx = state.head[0] * BOARD_DIMENSION + state.head[1]
+    $('.head').removeClass('head')
+    $(`.block-container :nth-child(${divIdx + 1})`).toggleClass('head')
+  }
+
+  ticker.add(tick)
+
+  $('#start-button').on('click', () => {
+    ticker.start()
+  })
+  $('#stop-button').on('click', () => {
+    ticker.stop()
+  })
+})
