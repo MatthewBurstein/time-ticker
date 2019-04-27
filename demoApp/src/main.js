ceccbcc679762c6a65fe035db0de0f0aad2795f3
Matt Burstein <j.matthew.burstein@gmail.com>
2019-04-27 19:11:45 +0100

    feat: handle arbitrary length body

--- demoApp/src/main.js
+++ demoApp/src/main.js
@@ -17,8 +17,9 @@ const BOARD_DIMENSION = 9
 const $ = require('jquery')
 
 const state = {
-  body: [{x: 0, y: 0}],
-  direction: ''
+  body: [{ x: 3, y: 0 }, { x: 2, y: 0 }, { x: 1, y: 0 }, { x: 0, y: 0 }],
+  direction: '',
+  maxLength: 4
 }
 
 
@@ -28,20 +29,8 @@ $('window').ready(() => {
   })
 
   const move = () => {
-    switch (state.direction) {
-      case 'left':
-      state.body[0].x = state.body[0].x === 0 ? BOARD_DIMENSION - 1 : state.body[0].x - 1
-      break;
-      case 'up':
-      state.body[0].y = state.body[0].y === 0 ? BOARD_DIMENSION - 1 : state.body[0].y - 1
-      break;
-      case 'right':
-      state.body[0].x = state.body[0].x === BOARD_DIMENSION - 1 ? 0 : state.body[0].x + 1
-      break;
-      case 'down':
-      state.body[0].y = state.body[0].y === BOARD_DIMENSION - 1 ? 0 : state.body[0].y + 1
-      break;
-    }
+    state.body.unshift(nextHead())
+    if (state.body.length > state.maxLength) { state.body.pop() }
   }
 
   const tick = () => {
@@ -50,9 +39,8 @@ $('window').ready(() => {
   }
 
   const render = () => {
-    const divIdx = state.body[0].y * BOARD_DIMENSION + state.body[0].x
     $('.head').removeClass('head')
-    $(`.block-container :nth-child(${divIdx + 1})`).toggleClass('head')
+    state.body.forEach(coords => $squareFromCoords(coords).addClass('head'))
   }
 
   ticker.add(tick)
@@ -64,3 +52,27 @@ $('window').ready(() => {
     ticker.stop()
   })
 })
+
+const $squareFromCoords = ({x, y}) => {
+  const divIdx = y * BOARD_DIMENSION + x
+  return $(`.block-container :nth-child(${divIdx + 1})`)
+}
+
+const nextHead = () => {
+  let {x, y} = state.body[0]
+  switch (state.direction) {
+      case 'left':
+      x = state.body[0].x === 0 ? BOARD_DIMENSION - 1 : state.body[0].x - 1
+    break;
+      case 'up':
+      y = state.body[0].y === 0 ? BOARD_DIMENSION - 1 : state.body[0].y - 1
+    break;
+      case 'right':
+      x = state.body[0].x === BOARD_DIMENSION - 1 ? 0 : state.body[0].x + 1
+    break;
+      case 'down':
+      y = state.body[0].y === BOARD_DIMENSION - 1 ? 0 : state.body[0].y + 1
+    break;
+  }
+  return {x, y}
+}
