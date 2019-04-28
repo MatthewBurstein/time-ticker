0f79bfdef6edfbfb26a7e14120bc06cadd527ff4
Matt Burstein <j.matthew.burstein@gmail.com>
2019-04-28 16:06:18 +0100

    feat: extract direction.js

---
+++ demoApp/src/direction.js
@@ -0,0 +1,32 @@
+export default class Direction {
+  constructor() {
+    this.dict = {
+      // arrows
+      37: 'left',
+      38: 'up',
+      39: 'right',
+      40: 'down',
+      // wasd
+      65: 'left',
+      87: 'up',
+      68: 'right',
+      83: 'down'
+    }
+    this.current = ''
+  }
+
+  setCurrent(keyCode) {
+    this.current = this.dict[keyCode]
+  }
+
+  isValid(keyCode) {
+    if (!this.dict.hasOwnProperty(keyCode)) { return false }
+
+    if (this.current === 'left' && this.dict[keyCode] === 'right')  { return false }
+    if (this.current === 'right' && this.dict[keyCode] === 'left')  { return false }
+    if (this.current === 'up' && this.dict[keyCode] === 'down')  { return false }
+    if (this.current === 'down' && this.dict[keyCode] === 'up')  { return false }
+
+    return true
+  }
+}
--- demoApp/src/main.js
+++ demoApp/src/main.js
@@ -4,40 +4,26 @@ const $ = require('jquery')
 
 import ticker from "../../src/index"
 import Snake from './snake'
+import Direction from './direction'
 import {boardDimension} from './appConstants'
 import { $squareFromCoords, createBoard, randomCoords } from './boardUtils'
 
 window.ticker = ticker
 ticker.setPeriod(500)
 
-const DIRECTIONS = {
-  // arrows
-  37: 'left',
-  38: 'up',
-  39: 'right',
-  40: 'down',
-  // wasd
-  65: 'left',
-  87: 'up',
-  68: 'right',
-  83: 'down'
-}
+const direction = new Direction()
 
 const snake = new Snake()
 
 const FOOD_PROBABILITY = 0.2
 
-const state = {
-  direction: 'right',
-  maxLength: 4
-}
-
-
 $('window').ready(() => {
   createBoard()
 
   $('.page-container').keydown(function(e) {
-    state.direction = DIRECTIONS[e.keyCode]
+    if (direction.isValid(e.keyCode)) {
+      direction.setCurrent(e.keyCode)
+    }
   })
 
   const generateFood = () => {
@@ -53,7 +39,7 @@ $('window').ready(() => {
   }
 
   const tick = () => {
-    snake.move(state.direction)
+    snake.move(direction.current)
     generateFood()
     render()
   }
